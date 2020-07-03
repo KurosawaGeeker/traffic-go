7c83f710571126610fb3ed6a5f1952f47217c17c
KurosawaGeeker <44112348+KurosawaGeeker@users.noreply.github.com>
2020-07-03 23:15:56 +0800

    Update models.go

--- models/models.go
+++ models/models.go
@@ -46,5 +46,5 @@ type User struct{
 func migration() {
 	// 自动迁移模式
 	DB.Set("gorm:table_options", "charset=utf8mb4").
-		AutoMigrate(&Location{}, &Badpic{}, &Goodpic{}，&User{})
+		AutoMigrate(&Location{}, &Badpic{}, &Goodpic{},&User{})
 }
