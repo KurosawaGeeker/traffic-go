7fa96acac2bd870f9736efdb8afaae05ae9c8024
KurosawaGeeker <44112348+KurosawaGeeker@users.noreply.github.com>
2020-07-03 23:05:44 +0800

    Update router.go

--- router/router.go
+++ router/router.go
@@ -1,24 +1,24 @@
 package router
 
 import (
-	"net/http"
 	"traffic-go/controllers"
-	"traffic-go/middlewares"
-
+	"traffic-go/middleware/jwt"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter() *gin.Engine {
 	router := gin.Default()
-	router.Use(middlewares.Cors())
-
-	router.StaticFS("/img", http.Dir("./static/img"))
-
 	v1 := router.Group("api/v1")
+	v1.Use(jwt.JWTAuth())  //中间件 需要经过登录验证的
 	{
 		v1.POST("/records", controllers.GetRecords)
-		v1.GET("/pictures", controllers.GetPictures)
-		v1.POST("/pictures", controllers.SetPicture)
+	}
+	v2 := router.Group("api/v2")
+	{
+		v2.GET("/pictures", controllers.GetPictures)
+		v2.POST("/pictures", controllers.SetPicture)
+		v2.POST("/auth",controllers.LoginUser)
+		v2.POST("/register",controllers.RegisterUser)
 	}
 	return router
 }
